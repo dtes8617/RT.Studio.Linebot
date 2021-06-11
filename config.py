2e0d3b2813de1039058e1a1a2313db8b54129269
Jude Su <fortunatemaker2603@gmail.com>
2021-06-11 18:41:41 +0800

    add google calendar password config. (#1)

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ config_parser.read('config.ini')
 
 class Config:
     google_calendar_username = config_parser.get('google calendar', 'AccountID')
+    google_calendar_password = config_parser.get('google calendar', 'AccountPassword')
 
 
 config = Config()
