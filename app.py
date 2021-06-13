ae9c9c5ccd25f58c7973dba2aea2b4fee52c7669
Jude Su <fortunatemaker2603@gmail.com>
2021-06-13 13:06:37 +0800

    hotfix: echoing user's words.

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from config import config
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(config.linebot_access_token)
 handler = WebhookHandler(config.linebot_secret)
+
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -26,6 +28,17 @@ def callback():
         abort(400)
 
     return 'OK'
+
+
+# 學你說話
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
 
 if __name__ == "__main__":
     app.run()
